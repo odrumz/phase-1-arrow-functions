1ac68a430ae048cd6251c89e538351e7d31460d1
DerrickOngoma <Odrumz64@gmail.com>
2024-03-20 14:24:37 +0300

    arrow function

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = (a,b) => a/b
+divide (2000, 100)
+    console.log(divide)
+
+const square = (x) => x*x
+square (2)
+ console.log(square)
+
+ const add = (a,b) => a+b
+ add(3,4)
+ console.log(add)
